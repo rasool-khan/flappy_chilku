d027a1443d0f7e607822c74aa5d7194aca5663a1
Sushan <sushan.gov@gmail.com>
2026-03-24 20:17:24 +0530

    updated api key and added another leaderboard

---
+++ firebase-config.example.js
@@ -0,0 +1,13 @@
+window.FLAPPY_CHILKU_CONFIG = {
+  firebase: {
+    apiKey: "PASTE_YOUR_API_KEY",
+    authDomain: "PASTE_YOUR_AUTH_DOMAIN",
+    databaseURL: "PASTE_YOUR_DATABASE_URL",
+    projectId: "PASTE_YOUR_PROJECT_ID",
+    storageBucket: "PASTE_YOUR_STORAGE_BUCKET",
+    messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID",
+    appId: "PASTE_YOUR_APP_ID",
+    measurementId: "PASTE_YOUR_MEASUREMENT_ID"
+  },
+  path: "leaderboard"
+};
